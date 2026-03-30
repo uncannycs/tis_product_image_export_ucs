3d288a63f1a298abe4c0784cab2f2ea36c48c08f
ucs <akodooucs@gmail.com>
2026-03-30 21:59:18 +0530

    [Index File Update]CM1 Updated index file ,icons and logos in 18.0

---
+++ tis_product_image_export/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    ODOO Open Source Management Solution
+#
+#    ODOO Addon module by Uncanny Consulting Services LLP
+#    Copyright (C) 2025 Uncanny Consulting Services LLP (<https://uncannycs.com>).
+#
+##############################################################################
+{
+    "name": 'Product Image Export',
+    "version": '18.0.1.0.0',
+    "sequence": 1,
+    "category": 'Inventory',
+    "summary": 'Product Image Export',
+    "website": "https://uncannycs.com",
+    "author": "Uncanny Consulting Services LLP",
+    "maintainers": "Uncanny Consulting Services LLP",
+    'description': """Product Image Export
+        """,
+    'depends': ['stock',
+                ],
+
+    'data': [
+        'report/product_report.xml',
+        'wizard/product_details_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'images': ["static/description/banner.gif"],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
