4e7c6ddc1f167596ba32a03c1dc310a862c98b9d
Uncanny Consulting Services LLP <82493780+uncannycs@users.noreply.github.com>
2026-07-06 17:34:48 +0530

    Delete tis_product_image_export directory

--- tis_product_image_export/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -- coding: utf-8 --
-# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
-# - © Technaureus Info Solutions Pvt. Ltd 2019. All rights reserved.
-
-from . import product_details
